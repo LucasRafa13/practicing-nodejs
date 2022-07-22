06e43a10c0fb21ba83ce3a1c58f2ec71bcf9d6c3
Lucas <lucasrafael123araujolima@gmail.com>
2022-07-22 00:34:19 -0400

    feat: :zap: Finalizando Rotas

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,14 @@ let db = new NeDB({
 })
 
 module.exports = app => {
-  app.get("/users", (req, res) => {
+  let route = app.route("/users")
+
+  route.get((req, res) => {
     db.find({})
       .sort({ name: 1 })
       .exec((err, users) => {
         if (err) {
-          console.log(`error: ${err}`)
-          res.status(400).json({
-            error: err
-          })
+          app.utils.error.send(err, req, res)
         } else {
           res.statusCode = 200
           res.setHeader("Content-Type", "application/json")
@@ -24,15 +23,48 @@ module.exports = app => {
       })
   })
 
-  app.post("/users", (req, res) => {
+  route.post((req, res) => {
+    if (!app.utils.validator.user(app, req, res)) return false
+
     db.insert(req.body, (err, user) => {
       if (err) {
-        console.log(`erro: ${err}`)
-        res.status(400).json({
-          error: err
-        })
+        app.utils.error.send(err, req, res)
       } else {
         res.status(200).json(user)
+      }
+    })
+  })
+
+  let routeId = app.route("/users/:id")
+
+  routeId.get((req, res) => {
+    db.findOne({ _id: req.params.id }).exec((err, user) => {
+      if (err) {
+        app.utils.error.send(err, req, res)
+      } else {
+        res.status(200).json(user)
+      }
+    })
+  })
+
+  routeId.put((req, res) => {
+    if (!app.utils.validator.user(app, req, res)) return false
+
+    db.update({ _id: req.params.id }, req.body, err => {
+      if (err) {
+        app.utils.error.send(err, req, res)
+      } else {
+        res.status(200).json(Object.assign(req.params, req.body))
+      }
+    })
+  })
+
+  routeId.delete((req, res) => {
+    db.remove({ _id: req.params.id }, {}, err => {
+      if (err) {
+        app.utils.error.send(err, req, res)
+      } else {
+        res.status(200).json(req.params)
       }
     })
   })
