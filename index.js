9be4a8d16775b3b10a7ad3ed6b8394f42d866bd8
Lucas <lucasrafael123araujolima@gmail.com>
2022-07-21 23:58:48 -0400

    feat: :fire:

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-consign().include("routes").include('utils').into(app)
+consign().include("routes").include("utils").into(app)
 
 app.listen(3000, "127.0.0.1", () => {
-  console.log("servidor rodando")
+  console.log("servidor rodando!")
 })
